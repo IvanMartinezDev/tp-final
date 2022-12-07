366880f64e4b599773636808982e26a6b5151149
IvanMartinezDev <martinezeliasivanb@gmail.com>
2022-12-06 22:36:02 -0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,14 @@
+const age = prompt("Ingrese su edad");
+const postalCode = prompt("Ingrese su código postal.")
+
+if(age < 18) {
+    alert("Al no ser mayor, ingresa a este sitio bajo su propia responsabilidad.");
+}
+
+function shippingCost() {
+    if(postalCode == 2900) {
+        alert("El envio será gratis");
+    } else {
+        alert("El envio le costará $1000")
+    }
+}
